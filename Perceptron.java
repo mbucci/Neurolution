e502bb463828d5544cb5fca5fdf2a9d8be71d028
mbucci <mbucci@bowdoin.edu>
2015-05-10 13:52:21 -0400

    ability to change weights of exiting network

--- Perceptron.java
+++ Perceptron.java
@@ -29,21 +29,38 @@ public class Perceptron
         this.rand = new Random();
     }
 
+    //Use this when initializing from scratch
     public void initWeights(int numInput, int numOutput) {
         this.initWeights(numInput, numOutput, null);
     }
 
+    //Use this to initialize with a set of weights
     public void initWeights(int numInput, int numOutput, double[] weights) {
-        this.biasNodeID = rand.nextInt(numInput);
+        this.initWeights(numInput, numOutput, weights, true);
+    }
+
+    //Use this to change the weights of an existing network
+    public void changeWeights(int numInput, int numOutput, double[] weights) {
+        this.initWeights(numInput, numOutput, weights, false);
+    }
+
+    //Don't use this
+    public void initWeights(int numInput, int numOutput, double[] weights, boolean newNet) {
+
+        if (newNet) this.biasNodeID = rand.nextInt(numInput);
+
         for (int i = 0; i < numInput; i++) {
             
             List<Edge> edgeList = new ArrayList<Edge>();
             for (int j = 0; j < numOutput; j++) {
                 double value;
-                if (weights == null) {
-                    //do weight initiliazation from weight ranges
-                    value = getRandomWeight();
-                } else value = weights[numOutput*i + j];
+                if (weights == null) value = getRandomWeight();      //initialize from weight ranges
+                else value = weights[numOutput*i + j];
+
+                if (!newNet) {
+                    this.setWeight(i, j, value);
+                    return;
+                }
 
                 Edge newEdge = new Edge(i, j, value);
                 edgeList.add(newEdge);
@@ -68,5 +85,9 @@ public class Perceptron
         double value = this.rand.nextDouble() * WEIGHT_HIGH;
         value -= WEIGHT_OFF;
         return value;
+    }
+
+    private void setWeight(int inID, int outID, double newWeight) {
+        this.network.get(inID).get(outID).setWeight(newWeight);
     }
 }
